af55b2da1879b2604536fe887e45c2dcfd50ee89
Zanila-drip <mleo0499@gmail.com>
2024-08-30 08:25:17 -0600

    Conditionals on python

---
+++ condicionales.py
@@ -0,0 +1,8 @@
+print("Bienvenido al bar Margaritas!")
+print("Como sabras, si eres menor de 18 edad, no tienes permitido entrar asi que dime cual es tu edad?")
+edad = int(input())
+
+if edad < 18:
+    print("No tienes permiso para entrar!")
+else:
+    print("Pasa adelanta mijo!")
